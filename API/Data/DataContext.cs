d10e549d974947d83359cb4be8b210b07ae41099
Vedant <vedantsharma.dbz23@gmail.com>
2023-11-15 11:59:11 +0530

    End of section 2

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; } //Users will be the name of the table in our database. Basically this property denotes the table in our db.
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API;
+
+public class AppUser
+{
+
+    public int Id { get; set; } //because of the name Id , E.F is going to automatically use this as the primary key :)
+    public string UserName { get; set;}
+    
+}
